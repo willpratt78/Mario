bc2594f9031e7841d816de0d53c1fc2c6eec1c63
William Pratt <willpratt78@gmail.com>
2022-05-27 12:24:32 -0700

    flesh out the rest of product_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,26 @@ class ProductsController < ApplicationController
     render :edit
   end
 
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :cost, :country_of_origin)
