e121a0e5f08621f99300ebce71a2de358f927f32
William Pratt <willpratt78@gmail.com>
2022-05-27 11:51:58 -0700

    create index, new, create and private

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
+end
